310da98d6db78a49de9f049f3086d46a4768b446
T I M M Y <105170187+Timmydudew@users.noreply.github.com>
2022-10-03 02:08:27 +0100

    Create Ai.py

---
+++ Ai.py
@@ -0,0 +1,22 @@
+import time
+import datetime
+import os
+import subprocess
+
+
+
+os.system('screenfetch')
+print('\033[1m' + '\033[91m'+'     ///   CREATED BY Timmy   ///')
+def wish():
+  h=int(datetime.datetime.now().hour)
+  if h<12:
+     subprocess.call(["termux-tts-speak","Good morning sir","I am JARVIS how may I help you"])
+  else:
+     if h>=12 and h<17:
+       subprocess.call(["termux-tts-speak","good afternoon sir","how may i help you"])
+     elif h>=17 and h<20:
+       subprocess.call(["termux-tts-speak","good evening sir,how may i help you"])
+     else:
+       subprocess.call(["termux-tts-speak","welcome sir how may i help you"])
+wish()
+os.system("python main.py")
